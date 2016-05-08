2441ee7347456eb810916b1aec65fb61aae69a72
Kamille Delgardo <kamilledelgardo@gmail.com>
2016-05-08 03:20:27 -0700

    Challenge 2: Install Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  get '/pages/home'
-
-  # You can have the root of your site routed with "root"
   root 'pages#home'
+  devise_for :users
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
